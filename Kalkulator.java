fe86186d65c6ed5a0a9088e804370c9381d5c612
t3rr41n <ivanderwi24@gmail.com>
2023-09-05 18:40:42 +0700

    test

---
+++ Kalkulator.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		float a, b;
+		
+		Scanner scn = new Scanner(System.in);
+		
+		System.out.println("Silahkan masukan nilai a :");
+		a = scn.nextFloat();
+		System.out.println("Silahkan masukan nilai b :");
+		b = scn.nextFloat();
+		
+		scn.close();
+		
+		System.out.printf("Isi variabel a adalah %f\n", a);
+		System.out.printf("Isi variabel b adalah %f\n", b);
+		System.out.printf("Hasil penjumlahan variabel a dan b = %f\n", a + b);
+		System.out.printf("Hasil pengurangan variabel a dan b = %f\n", a - b);
+		System.out.printf("Hasil perkalian variabel a dan b = %f\n", a * b);
+		System.out.printf("Hasil pembagian variabel a dan b = %f\n", a / b);
+	}
+}
